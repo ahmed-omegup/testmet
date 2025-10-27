064dffad5590c19758ad968822ddf795c0fb41ba
ahmed <ahmed>
2025-10-27 16:01:50 +0100

    wip

--- benchmark/meteor/server/seed.js
+++ benchmark/meteor/server/seed.js
@@ -1,12 +1,13 @@
 import { Meteor } from "meteor/meteor";
 import { Docs } from "./publications.js";
 
-Meteor.startup(() => {
-  if (Docs.find().count() === 0) {
+Meteor.startup(async () => {
+  const count = await Docs.find().countAsync();
+  if (count === 0) {
     const N = 1_000_000;
     console.log(`Seeding ${N} documents`);
     for (let i = 0; i < N; i++) {
-      Docs.insert({
+      await Docs.insertAsync({
         _id: i,
         name: `doc_${i}`,
         score: Math.random() * 1000,
