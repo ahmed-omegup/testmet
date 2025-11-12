1f1c06e6b9ec935776456d63e5b734411fb15734
user <email>
2025-11-12 21:24:18 +0100

    wip

--- btree-pubsub/src/replication.rs
+++ btree-pubsub/src/replication.rs
@@ -138,13 +138,25 @@ async fn process_decoderbufs_message(
             }
         }
         decoderbufs::Op::Update => {
-            if let Some((id, score)) = extract_id_and_score(&row_msg.new_tuple) {
-                handle_update(id, score, range_index, storage, registry).await;
+            let old = extract_id_and_score(&row_msg.old_tuple);
+            let newv = extract_id_and_score(&row_msg.new_tuple);
+            match (newv, old) {
+                (Some((id_new, new_score)), Some((_id_old, old_score))) => {
+                    // Prefer id from new tuple
+                    handle_update(id_new, Some(old_score), new_score, range_index, storage, registry).await;
+                }
+                (Some((id, new_score)), None) => {
+                    handle_update(id, None, new_score, range_index, storage, registry).await;
+                }
+                _ => {}
             }
         }
         decoderbufs::Op::Delete => {
-            if let Some(id) = extract_id(&row_msg.old_tuple) {
-                handle_delete(id, range_index, storage, registry).await;
+            if let Some((id, old_score)) = extract_id_and_score(&row_msg.old_tuple) {
+                handle_delete(id, old_score, range_index, storage, registry).await;
+            } else if let Some(id) = extract_id(&row_msg.old_tuple) {
+                // If score missing, we can't adjust counts correctly; skip index update but notify removals based on tracking
+                handle_delete_no_value(id, range_index, storage, registry).await;
             }
         }
         decoderbufs::Op::Begin | decoderbufs::Op::Commit => {
@@ -206,92 +218,130 @@ async fn handle_insert(
     id: String,
     score: i32,
     range_index: &Arc<RwLock<RangeQueryIndex>>,
-    storage: &Arc<SubscriptionStore>,
+    _storage: &Arc<SubscriptionStore>,
     registry: &Arc<ConnectionRegistry>,
 ) {
     // Get queries this document matches
     let matches = {
         let mut index = range_index.write().await;
-        index.get_queries_for_document(&id, score)
+        index.get_queries_for_change(&id, None, score as f64, 1)
     };
-    
-    // Notify connections for each query the document was added to
+
+    // Notify all subscribed connections for each query
     for query_id in matches.added_to {
-        let index = range_index.read().await;
-        if let Some(query) = index.get_query(&query_id) {
-            info!("Notifying connection {} (query {}) about INSERT: {}", query.connection_id, query_id, id);
+        let conns = {
+            let index = range_index.read().await;
+            index.get_connections_for_query(&query_id)
+        };
+        for conn_id in conns {
+            info!("Notifying connection {} (query {}) about INSERT: {}", conn_id, query_id, id);
             let notification = Notification::Added {
                 query_id: query_id.clone(),
                 id: id.clone(),
                 score,
             };
-            registry.notify(&query.connection_id, notification).await;
+            registry.notify(&conn_id, notification).await;
         }
     }
 }
 
 async fn handle_update(
     id: String,
+    old_score: Option<i32>,
     new_score: i32,
     range_index: &Arc<RwLock<RangeQueryIndex>>,
-    storage: &Arc<SubscriptionStore>,
+    _storage: &Arc<SubscriptionStore>,
     registry: &Arc<ConnectionRegistry>,
 ) {
     // Get queries this document was added to or removed from
     let matches = {
         let mut index = range_index.write().await;
-        index.get_queries_for_document(&id, new_score)
+        index.get_queries_for_change(&id, old_score.map(|s| s as f64), new_score as f64, 1)
     };
-    
+
     // Notify about additions
     for query_id in matches.added_to {
-        let index = range_index.read().await;
-        if let Some(query) = index.get_query(&query_id) {
-            info!("Notifying connection {} (query {}) about ADD (update): {}", query.connection_id, query_id, id);
+        let conns = {
+            let index = range_index.read().await;
+            index.get_connections_for_query(&query_id)
+        };
+        for conn_id in conns {
+            info!("Notifying connection {} (query {}) about ADD (update): {}", conn_id, query_id, id);
             let notification = Notification::Added {
                 query_id: query_id.clone(),
                 id: id.clone(),
                 score: new_score,
             };
-            registry.notify(&query.connection_id, notification).await;
+            registry.notify(&conn_id, notification).await;
         }
     }
-    
+
     // Notify about removals
     for query_id in matches.removed_from {
-        let index = range_index.read().await;
-        if let Some(query) = index.get_query(&query_id) {
-            info!("Notifying connection {} (query {}) about REMOVE (update): {}", query.connection_id, query_id, id);
+        let conns = {
+            let index = range_index.read().await;
+            index.get_connections_for_query(&query_id)
+        };
+        for conn_id in conns {
+            info!("Notifying connection {} (query {}) about REMOVE (update): {}", conn_id, query_id, id);
             let notification = Notification::Removed {
                 query_id: query_id.clone(),
                 id: id.clone(),
             };
-            registry.notify(&query.connection_id, notification).await;
+            registry.notify(&conn_id, notification).await;
         }
     }
 }
 
 async fn handle_delete(
     id: String,
+    old_score: i32,
     range_index: &Arc<RwLock<RangeQueryIndex>>,
-    storage: &Arc<SubscriptionStore>,
+    _storage: &Arc<SubscriptionStore>,
     registry: &Arc<ConnectionRegistry>,
 ) {
-    // Get all queries this document was in and notify removal
+    // Remove from index and get queries it was part of
     let query_ids = {
         let mut index = range_index.write().await;
-        index.remove_document(&id)
+        index.remove_document(&id, old_score as f64, 1)
     };
-    
+
     for query_id in query_ids {
-        let index = range_index.read().await;
-        if let Some(query) = index.get_query(&query_id) {
-            info!("Notifying connection {} (query {}) about DELETE: {}", query.connection_id, query_id, id);
+        let conns = {
+            let index = range_index.read().await;
+            index.get_connections_for_query(&query_id)
+        };
+        for conn_id in conns {
+            info!("Notifying connection {} (query {}) about DELETE: {}", conn_id, query_id, id);
             let notification = Notification::Removed {
                 query_id: query_id.clone(),
                 id: id.clone(),
             };
-            registry.notify(&query.connection_id, notification).await;
+            registry.notify(&conn_id, notification).await;
+        }
+    }
+}
+
+async fn handle_delete_no_value(
+    id: String,
+    range_index: &Arc<RwLock<RangeQueryIndex>>,
+    _storage: &Arc<SubscriptionStore>,
+    registry: &Arc<ConnectionRegistry>,
+) {
+    // We can't adjust counts. Only notify based on tracked memberships.
+    let query_ids = {
+        let index = range_index.read().await;
+        // No direct API to fetch doc's queries; using remove with dummy doesn't work. Skip.
+        Vec::<String>::new()
+    };
+    for query_id in query_ids {
+        let conns = {
+            let index = range_index.read().await;
+            index.get_connections_for_query(&query_id)
+        };
+        for conn_id in conns {
+            let notification = Notification::Removed { query_id: query_id.clone(), id: id.clone() };
+            registry.notify(&conn_id, notification).await;
         }
     }
 }
